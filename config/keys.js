f2b09ae24b0ad4cb1a5fae10490ea1c3be62d0a5
Gamygams1234 <gamyburgos@gmail.com>
2020-08-14 13:42:34 -0700

    Starting our server and adding our react scripts

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://GamalielBurgos:744ZDOhuov7xpPeL@Cluster0.70s70.mongodb.net/chatroom_1?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const uri = require("./config/keys").mongoURI;
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+// mongoose
+//   .connect(db)
+//   .then(() => console.log("MongoDB Connected..."))
+//   .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
