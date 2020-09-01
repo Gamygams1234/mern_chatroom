fc334c27db57f4bdce7e1b554f6efc406f05b0fa
Gamygams1234 <gamyburgos@gmail.com>
2020-09-01 00:06:02 -0700

    Post reducer and post item component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profiles/Profile";
+import Posts from "./posts/Posts";
+
 // this is for redux
 
 import { Provider } from "react-redux";
@@ -48,6 +50,7 @@ const App = () => {
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
               <PrivateRoute exact path="/add-experience" component={AddExperience} />
               <PrivateRoute exact path="/add-education" component={AddEducation} />
+              <PrivateRoute exact path="/posts" component={Posts} />
             </Switch>
           </section>
         </Fragment>
